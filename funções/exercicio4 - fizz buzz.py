ebc179d9ddd7b61f0d50d4e298bb9a25babaed24
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-20 18:23:12 -0300

    exercicio 4 - fizz buzz

---
+++ funções/exercicio4 - fizz buzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 5 == 0:
+        print('Buzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    else:
+        return num
+
+num = int(input('Digite um número: '))
+
+fizzbuzz(num)
