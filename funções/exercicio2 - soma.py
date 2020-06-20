fa9be1d53c0e2fefc524be3ccc1f1c943245904c
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-20 17:51:13 -0300

    exercicio 2 - soma

---
+++ funções/exercicio2 - soma.py
@@ -0,0 +1,9 @@
+def somar(n1, n2, n3):
+    return n1 + n2+ n3
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+
+soma = somar(n1, n2, n3)
+print(f'Resultado: {soma}')
