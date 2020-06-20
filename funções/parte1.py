c5e990377e80b1ba3e818b372416b22436e3af76
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-20 16:48:45 -0300

    outro exemplo

--- funções/parte1.py
+++ funções/parte1.py
@@ -1,3 +1,4 @@
+"""
 num1 = int(input('Digite um número: '))
 num2 = int(input('Digite outro: '))
 
@@ -6,3 +7,11 @@ def somar(num1, num2):
     print(f'Resultado: {soma}')
 
 somar(num1, num2)
+"""
+def saudacao(msg, nome):
+    print(msg, nome)
+
+msg = 'Olá,'
+nome = input('Digite seu nome: ')
+
+saudacao(msg, nome)
