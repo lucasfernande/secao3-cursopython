6f5a057e60cb44f89accd14fab9ea71a0185fae8
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-22 13:00:51 -0300

    função comum para ordenar lista

--- expressoes-lambda/lambda.py
+++ expressoes-lambda/lambda.py
@@ -1,3 +1,19 @@
+"""
 func = lambda x, y: x + y
 
 print(func(2, 3))
+"""
+
+produtos = [
+    ['Celular', 1100.00],
+    ['TV', 1900.00],
+    ['Microondas', 290.00],
+    ['Ventilador', 120.00],
+    ['Liquidificador', 100.00]
+]
+
+def ordenar(item):
+    return item[1]
+
+produtos.sort(key = ordenar, reverse = True)
+print(produtos)
