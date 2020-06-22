b588bb2ec11ab58d99d3db860430ac395f4a5c79
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-22 13:18:33 -0300

    convertendo tuplas e listas

--- tuplas/tuplas.py
+++ tuplas/tuplas.py
@@ -1,4 +1,4 @@
-
+"""
 t0 = 1, 2, 3
 t1 = 4, 5, 6
 t2 = t0 + t1
@@ -6,3 +6,13 @@ t2 = t0 + t1
 print(t0)
 print(t1)
 print(t2)
+"""
+
+t1 = 1, 2, 3, 4, 5
+t1 = list(t1)
+
+t1[1] = 20
+print(t1)
+
+t1 = tuple(t1)
+print(t1)
