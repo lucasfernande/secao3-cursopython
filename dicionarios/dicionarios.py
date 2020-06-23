95862c0dc91dafb791524718d6f674cda637e0c6
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-23 13:02:31 -0300

    outra forma de criar dicionários

--- dicionarios/dicionarios.py
+++ dicionarios/dicionarios.py
@@ -1,4 +1,10 @@
+"""
 dicio = {'nomedaChave':'valor da chave'}
 dicio['outra_chave'] = 'valor da outra chave'
 
+print(dicio)
+print(dicio['nomedaChave'])
+"""
+
+dicio = dict(chave = 'valor da chave', chave2 = 'valor da chave2')
 print(dicio)
