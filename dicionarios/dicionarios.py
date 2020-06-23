de968c398239400d5cd55960f822f2634838745c
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-23 13:33:58 -0300

    iterando sobre um dicionário

--- dicionarios/dicionarios.py
+++ dicionarios/dicionarios.py
@@ -22,8 +22,19 @@ print(dicio)
 
 """
 
-print('chave2' in dicio)
-print('chave2' in dicio.keys())
-print('valor da chave' in dicio.values())
+"""
+print('chave2' in dicio) 
+print('chave2' in dicio.keys()) # checando se chave2 existe nas chaves de dicio
+print('valor da chave' in dicio.values()) # checando se valor da chave existe nos valores de dicio
 
 print(len(dicio))
+"""
+
+for key in dicio.keys(): # iterando sobre as chaves
+    print(key)
+
+for value in dicio.values(): # iterando sobre os valores
+    print(value)
+
+for items in dicio.items(): # iterando sobre chaves e valores
+    print(items)
