a2f6e5e4d76b72c71aac412761885b67e8460166
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-23 13:17:19 -0300

    método update em dicionários

--- dicionarios/dicionarios.py
+++ dicionarios/dicionarios.py
@@ -9,3 +9,8 @@ print(dicio['nomedaChave'])
 dicio = dict(chave = 'valor da chave', chave2 = 'valor da chave2')
 print(dicio)
 print(dicio.get('chave'))
+
+dicio.update({'chave':'outro valor'}) # atualizando uma chave
+dicio.update({'chave3':'valor da chave3'}) # adicionando uma nova chave
+
+print(dicio)
