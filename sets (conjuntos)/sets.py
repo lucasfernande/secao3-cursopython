4cf7e01fe5c70241f94166d1c8a59cd6d605de54
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-24 15:52:08 -0300

    usando set para remover valores repetidos de uma lista

--- sets (conjuntos)/sets.py
+++ sets (conjuntos)/sets.py
@@ -1,3 +1,4 @@
+"""
 set = {1, 2, 3, 4, 5, 6, 7}
 
 set.add(8)
@@ -15,3 +16,11 @@ print(set)
 set.update([9, 10, 11, 12])
 
 print(set)
+"""
+
+lista = [1, 2, 3, 3, 3, 4, 2, 2, 5, 5, 6, 7, 7, 7, 8, 9, 9, 10]
+
+lista = set(lista) # convertendo para set
+lista = list(lista) # voltando para lista
+
+print(lista)
