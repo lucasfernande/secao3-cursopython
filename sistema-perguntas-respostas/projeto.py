b74ab41f5c2c4f29fed268d1500e025b7e34deb6
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-24 14:12:34 -0300

    primeiras perguntas

---
+++ sistema-perguntas-respostas/projeto.py
@@ -0,0 +1,17 @@
+perguntas = {
+    'Pergunta 1' : {
+        'pergunta' : 'Quanto é 6x8?',
+        'respostas' : {'a' : '40', 'b' : '45', 'c' : '48', 'd' : '36'},
+        'resp_certa' : 'c',
+    },
+    'Pergunta 2' : {
+            'pergunta' : 'Em que ano foi promulgada a Constituição da República Federativa do Brasil?' ,
+            'respostas' : {'a' : '1984', 'b' : '1985', 'c' : '1990', 'd' : '1988'},
+            'resp_certa' : 'd',
+    },
+    'Pergunta 3' : {
+            'pergunta' : 'Qual o maior país do Mundo' ,
+            'respostas' : {'a' : 'Rússia', 'b' : 'Brasil', 'c' : 'Canadá' ,'d' : 'Estados Unidos'},
+            'resp_certa' : 'a',
+    },
+}
