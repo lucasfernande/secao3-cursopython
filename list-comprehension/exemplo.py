c71abb80eb68431c547fc4a7f5e2549562b5def1
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-26 12:39:51 -0300

    convertendo letras para maiusculo

--- list-comprehension/exemplo.py
+++ list-comprehension/exemplo.py
@@ -1,8 +1,15 @@
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+"""
 lista2 = [valor for valor in lista]
 
 print(lista2)
 
 listaDobro = [valor * 2 for valor in lista]
 print(listaDobro)
+"""
+
+nomes = ['lucas', 'maria', 'alex']
+conv = [nome.replace(nome[0], nome[0].upper()) for nome in nomes]
+
+print(conv)
