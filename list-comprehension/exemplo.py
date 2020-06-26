7e95795d7f3b8464c93da89a58d1809d51aff76f
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-26 12:50:21 -0300

    filtrando uma lista

--- list-comprehension/exemplo.py
+++ list-comprehension/exemplo.py
@@ -1,6 +1,6 @@
+"""
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-"""
 lista2 = [valor for valor in lista]
 
 print(lista2)
@@ -9,7 +9,13 @@ listaDobro = [valor * 2 for valor in lista]
 print(listaDobro)
 """
 
+"""
 nomes = ['lucas', 'maria', 'alex']
 conv = [nome.replace(nome[0], nome[0].upper()) for nome in nomes]
 
 print(conv)
+"""
+
+lista = list(range(100))
+filtro =  [valor for valor in lista if valor % 2 == 0]
+print(filtro)
