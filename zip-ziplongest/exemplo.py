b01e2c8ec47dc0996d189398362fb9114258b8ce
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-29 15:03:29 -0300

    zip longest

--- zip-ziplongest/exemplo.py
+++ zip-ziplongest/exemplo.py
@@ -1,7 +1,9 @@
-capitais = ['São Paulo', 'Salvador', 'Curitiba', 'Goiânia', 'Manaus']
+from itertools import zip_longest
+
+capitais = ['São Paulo', 'Salvador', 'Curitiba', 'Goiânia', 'Manaus', 'Rio Branco', 'Porto Alegre']
 
 estados = ['SP', 'BA', 'PA', 'GO', 'AM']
 
-cidades_estados = zip(estados, capitais)
+cidades_estados = zip_longest(estados, capitais) # o zip longest irá preencher o estado com none
 
 print(list(cidades_estados))
