c3603a6142b8e7b1f7d6b41a30eeb43866eb34f6
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-29 15:00:38 -0300

    zip - juntando duas listas

---
+++ zip-ziplongest/exemplo.py
@@ -0,0 +1,7 @@
+capitais = ['São Paulo', 'Salvador', 'Curitiba', 'Goiânia', 'Manaus']
+
+estados = ['SP', 'BA', 'PA', 'GO', 'AM']
+
+cidades_estados = zip(estados, capitais)
+
+print(list(cidades_estados))
