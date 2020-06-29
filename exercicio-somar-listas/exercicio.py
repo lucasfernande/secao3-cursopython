b59799e21203ee5aada37f86472513745f2fc9e7
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-29 15:44:13 -0300

    exercicio - somar listas - solução pythonica

--- exercicio-somar-listas/exercicio.py
+++ exercicio-somar-listas/exercicio.py
@@ -1,8 +1,11 @@
 listaA = [1, 2, 3, 4, 5, 6, 7]
 listaB = [1, 2, 3, 4]
 
-listaSoma = []
+"""
 for i in range(len(listaB)):
     listaSoma.append(listaB[i] + listaA[i])
 
+print(listaSoma)
+"""
+listaSoma = [x + y for x, y in zip(listaA, listaB)]
 print(listaSoma)
