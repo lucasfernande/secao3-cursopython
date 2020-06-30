f9b1f2a937d41309a9a72a59588a237f48dbe168
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-30 13:43:42 -0300

    correção - a ordem NÃO importa em combinations

--- combinations-permutations-product/exemplo.py
+++ combinations-permutations-product/exemplo.py
@@ -2,7 +2,7 @@ import itertools
 
 pessoas = ['João', 'Maria', 'Bob', 'Fernanda', 'Eduardo', 'Rosa']
 
-# com combinations, a ordem importa (ex: João e Maria é a mesma coisa que Maria e João)
+# com combinations, a ordem NÃO importa (ex: João e Maria é a mesma coisa que Maria e João)
 
 for grupo in itertools.combinations(pessoas, 2):
     print(grupo)
