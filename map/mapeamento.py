b107e34e96f066a9cd3ef18a8fc8419ebbaa535e
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-30 14:59:39 -0300

    acessando dicionário com map

--- map/mapeamento.py
+++ map/mapeamento.py
@@ -1,6 +1,13 @@
 import dados
 
+"""
 newList = list(map(lambda x: x * 2, dados.lista))
 
 print(dados.lista)
 print(newList)
+"""
+
+precos = list(map(lambda p: p['preco'], dados.produtos))
+
+for preco in precos:
+    print(preco)
