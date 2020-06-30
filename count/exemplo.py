56e19b64fd6298c4f23539ff5d62936ec629ac2f
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-30 13:29:21 -0300

    colocando indices em uma lista

--- count/exemplo.py
+++ count/exemplo.py
@@ -1,12 +1,19 @@
 from itertools import count
 
-contador = count(start = 10, step = -1)
+contador = count()
 
 # start = começo, step = de quanto em quanto ele vai pular
+lista = ['Luiz', 'Maria', 'João', 'Eduardo']
+lista = zip(contador, lista)
 
+print(list(lista))
+
+
+
+"""
 for v in contador:
     print(v)
     if v <= 0:
         break
-
+"""
 
