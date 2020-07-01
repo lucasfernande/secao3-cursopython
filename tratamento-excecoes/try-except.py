4a52bd9f45ca6ab5674a69a6d984c3e6b9fbf2da
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-01 13:52:37 -0300

    erro de indice

--- tratamento-excecoes/try-except.py
+++ tratamento-excecoes/try-except.py
@@ -1,4 +1,9 @@
 try:
-    print(a)
+    a = []
+    print(a[1])
 except NameError as erro:
     print('Erro: ', erro)
+except IndexError as erro:
+    print('Erro: ', erro)
+except Exception as erro:
+    print('Ocorreu um erro inesperado')
