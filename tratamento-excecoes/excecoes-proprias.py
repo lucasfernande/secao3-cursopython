d9a115afa6412709c2166703dafffe2cc3f4ba2f
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-01 14:29:37 -0300

    capturando e relançando exceção

--- tratamento-excecoes/excecoes-proprias.py
+++ tratamento-excecoes/excecoes-proprias.py
@@ -3,4 +3,11 @@ def divide(n1, n2):
         raise ValueError('O número não pode ser dividido por Zero')
     return n1 / n2
 
-print(divide(2, 0))
+n1 = 2
+n2 = 0
+
+try:
+    print(divide(n1, n2))
+except ValueError as error:
+    print(f'Não é possível dividir {n1} por zero')
+    print('Log: ', error)
