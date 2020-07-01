fdac479c7dd79f94a8e43a6d0db8c28a44dd52f4
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-01 13:55:20 -0300

    erro de chave em dicionários

--- tratamento-excecoes/try-except.py
+++ tratamento-excecoes/try-except.py
@@ -3,7 +3,7 @@ try:
     print(a[1])
 except NameError as erro:
     print('Erro: ', erro)
-except IndexError as erro:
-    print('Erro: ', erro)
+except (IndexError, KeyError) as erro:
+    print('Erro de índice ou chave: ', erro)
 except Exception as erro:
     print('Ocorreu um erro inesperado')
