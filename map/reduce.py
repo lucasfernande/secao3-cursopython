06c91d3a4dddd791d37ba8e6ba5ce95f44417030
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-01 13:40:28 -0300

    tirando média de idade com reduce

--- map/reduce.py
+++ map/reduce.py
@@ -6,7 +6,12 @@ somaLista = functools.reduce(lambda ac, i: i + ac, dados.lista, 0)
 
 print(somaLista)
 """
-
+"""
 somaPrecos = functools.reduce(lambda ac, p: p['preco'] + ac, dados.produtos, 0)
 
 print(round(somaPrecos / len(dados.produtos), 2))
+"""
+
+somaIdades = functools.reduce(lambda ac, i: i['idade'] + ac, dados.pessoas, 0)
+
+print(f'Média de Idade: {somaIdades / len(dados.pessoas)}')
