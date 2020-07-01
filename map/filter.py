6a1df9e02d4aeda5c37119574cb6ed0e9bc61ba4
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-01 12:53:23 -0300

    filtrando produtos por preço

--- map/filter.py
+++ map/filter.py
@@ -1,5 +1,10 @@
 import dados
 
-filtro = filter(lambda x: x > 4, dados.lista)
+# filtroComp = [x for x in dados.lista if x > 5] list comprehension
+# filtro = filter(lambda x: x > 4, dados.lista)
+# print(list(filtro))
 
-print(list(filtro))
+filtroProduto = filter(lambda p: p['preco'] > 45, dados.produtos)
+
+for prod in filtroProduto:
+    print(prod)
