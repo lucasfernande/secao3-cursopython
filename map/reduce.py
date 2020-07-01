f6c5195f4baaf6733a0ef35f5d179983cf0e1f2d
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-01 13:37:55 -0300

    tirando média de preços com reduce

--- map/reduce.py
+++ map/reduce.py
@@ -1,6 +1,12 @@
 import functools
 import dados
 
+"""
 somaLista = functools.reduce(lambda ac, i: i + ac, dados.lista, 0)
 
 print(somaLista)
+"""
+
+somaPrecos = functools.reduce(lambda ac, p: p['preco'] + ac, dados.produtos, 0)
+
+print(round(somaPrecos / len(dados.produtos), 2))
