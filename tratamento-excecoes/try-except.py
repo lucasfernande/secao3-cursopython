22d2998df858f95d5fad621108e4aa850ec80127
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-01 13:57:19 -0300

    bloco else

--- tratamento-excecoes/try-except.py
+++ tratamento-excecoes/try-except.py
@@ -1,9 +1,11 @@
 try:
     a = []
-    print(a[1])
+    print(a)
 except NameError as erro:
     print('Erro: ', erro)
 except (IndexError, KeyError) as erro:
     print('Erro de índice ou chave: ', erro)
 except Exception as erro:
     print('Ocorreu um erro inesperado')
+else:
+    print('Código executado com sucesso')
