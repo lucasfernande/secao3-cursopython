502ca33a6f0ec9d36353f3aa01bcb2e0e9fdb8a5
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-04 16:51:35 -0300

    importando todo de um modeulo

--- módulos-padrão/modulos.py
+++ módulos-padrão/modulos.py
@@ -2,5 +2,12 @@
 import sys
 print(sys.platform) # método que mostra o OS do usuário
 """
-from sys import platform as so
+"""
+from sys import platform as so # importando apenas uma função e dando um apelido
 print(so)
+"""
+
+from random import * # importando tudo do modulo
+
+for i in range(10):
+    print(random.randint(0, 10))
