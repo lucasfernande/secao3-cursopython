9948c9e6923df609369e7212f3757fe133615cae
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-07 13:59:32 -0300

    formatando preço

--- criando-pacotes/main.py
+++ criando-pacotes/main.py
@@ -4,6 +4,8 @@ from vendas import calculaPrecos
 preco = 49.90
 
 precoReajuste = calculaPrecos.reajusteAumenta(preco, 10)
+print(calculaPrecos.formata(precoReajuste))
 
-print(precoReajuste)
+precoReducao = calculaPrecos.reajusteAbaixa(preco, 10)
+print(calculaPrecos.formata(precoReducao))
 
--- criando-pacotes/vendas/calculaPrecos.py
+++ criando-pacotes/vendas/calculaPrecos.py
@@ -1,7 +1,10 @@
-def reajusteAumenta(preco, porcent):
+def reajusteAumenta(preco, porcent, formata = True):
     r = preco + (preco * (porcent / 100))
     return r
 
-def reajusteAbaixa(preco, porcent):
+def reajusteAbaixa(preco, porcent, formata = True):
     r = preco - (preco * (porcent / 100))
     return r
+
+def formata(preco):
+    return f'R$ {preco:.2f}'.replace('.', ',')
