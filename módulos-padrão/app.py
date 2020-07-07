b108940939e463c98e28dd073f4e5b330b2ec2ff
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-07 13:19:33 -0300

    criando módulos

---
+++ módulos-padrão/app.py
@@ -0,0 +1,6 @@
+import calc
+
+print(calc.soma(2, 1))
+print(calc.subt(2, 1))
+print(calc.mult(5, 2))
+print(calc.div(10, 2))
---
+++ módulos-padrão/calc.py
@@ -0,0 +1,11 @@
+def soma(n1, n2):
+    return n1 + n2
+
+def subt(n1, n2):
+    return n1 - n2
+
+def mult(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
