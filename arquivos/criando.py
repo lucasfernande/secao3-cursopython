f9d3fb13e9fece098f874a6cdb7916ffc4089ac3
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-07 14:25:41 -0300

    lendo linhas com for

--- arquivos/criando.py
+++ arquivos/criando.py
@@ -4,6 +4,7 @@ file.write('Linha\n')
 file.write('Outra linha\n')
 file.write('Outra linha')
 
+"""
 file.seek(0, 0) # movendo o cursor para o topo do arquivo para poder ler o arquivo
 print('Lendo arquivo: ')
 print(file.read())
@@ -13,5 +14,10 @@ file.seek(0, 0)
 print(file.readline(), end='') # end para não quebrar linha
 print(file.readline(), end='')
 print(file.readline())
+"""
+
+file.seek(0, 0)
+for line in file.readlines():
+    print(line, end='')
 
 file.close()
