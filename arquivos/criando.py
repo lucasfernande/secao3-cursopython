4be2f503aa672748b00ef3cd470087da69ee6aaf
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-07 14:09:16 -0300

    lendo o arquivo

--- arquivos/criando.py
+++ arquivos/criando.py
@@ -3,4 +3,10 @@ file = open('in.txt', 'w+') # w+ significa leitura e escrita
 file.write('Linha\n')
 file.write('Outra linha\n')
 file.write('Outra linha')
+
+file.seek(0, 0) # movendo o cursor para o topo do arquivo para poder ler o arquivo
+print('Lendo arquivo: ')
+print(file.read())
+print()
+
 file.close()
