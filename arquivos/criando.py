b328ee12c100a25d7cbc4e4501513670f3514647
lucasfernande <lucas.fernandes6192@outlook.com>
2020-07-07 14:49:40 -0300

    usando o gerenciador de contexto (maneira mais pythonica)

--- arquivos/criando.py
+++ arquivos/criando.py
@@ -1,10 +1,10 @@
+"""
 file = open('in.txt', 'w+') # w+ significa leitura e escrita
 
 file.write('Linha\n')
 file.write('Outra linha\n')
 file.write('Outra linha')
 
-"""
 file.seek(0, 0) # movendo o cursor para o topo do arquivo para poder ler o arquivo
 print('Lendo arquivo: ')
 print(file.read())
@@ -14,10 +14,18 @@ file.seek(0, 0)
 print(file.readline(), end='') # end para não quebrar linha
 print(file.readline(), end='')
 print(file.readline())
-"""
 
 file.seek(0, 0)
 for line in file.readlines():
     print(line, end='')
 
 file.close()
+"""
+
+with open('in.txt', 'w+') as file:
+    file.write('Linha\n')
+    file.write('Outra\n')
+    file.write('Outra Linha')
+
+    file.seek(0, 0)
+    print(file.read())
