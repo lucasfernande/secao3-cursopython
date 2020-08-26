5a56f000b2615d98385df7ef7dd6fcb17bb94708
lucasfernande <lucas.fernandes6192@outlook.com>
2020-08-26 13:25:57 -0300

    retornando uma função dentro de outra

--- funções-decoradoras(es)/exemplos.py
+++ funções-decoradoras(es)/exemplos.py
@@ -1,7 +1,18 @@
+"""""
 def fala_oi():
     print('oi')
 
 var = fala_oi
 
 print(type(var))
+var()
+"""""
+
+def master():
+    def slave():
+        print('oi')
+    return slave
+
+var = master()
+
 var()
